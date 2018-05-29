09315246408074cc42ee1282716aa5d97c827d72
dianajacobin <dianajacobin@gmail.com>
2018-05-29 06:00:56 -0500

    Ágregando logica en JS de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,62 @@
 window.cipher = {
-  // ... 
+
+encode : (string, Offset )=> {
+
+  let result = "",code;
+  let string =codigo.value;
+  let Offset =number.value;
+
+  for (let i=0; i<string.length;i++)
+  
+  {
+          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90)
+          { code = (((string.charCodeAt(i)-65)+Offset)%26)+65;}
+
+          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122)
+          { code = (((string.charCodeAt(i)-97)+Offset)%26)+97;}
+
+          else if (string.charCodeAt(i)===32) 
+          { code = 32;}
+          result+=String.fromCharCode(code);
+  }
+
+  return result;
+                },
+
+decode : ( string, Offset)=> {  
+  let result = "",code;
+  let string =codigo.value;
+  let Offset =number.value;
+  
+  for (let i=0; i<string.length;i++)
+    
+  {
+          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90)
+          { code = (((string.charCodeAt(i)-65)-n+52)%26)+65;}
+
+          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122)
+          { code = (((string.charCodeAt(i)-97)-n+52)%26)+97;}
+
+          else if (string.charCodeAt(i)===32) 
+          { code = 32;}
+          result+=String.fromCharCode(code);
+  }
+
+  return result;
+              },  
+
+  /*          
+
+    createCipherWithOffset: (Offset)=>
+    {
+          return 
+          {
+            encode: (string)=> 
+            {
+                  result;
+            }
+          }
+    }
+
 };
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 
-function getText() {
-document.getElementById(ingresoTexto).innerHTML=" ";
-}
+// acá debes escuchar eventos del DOM,
+// invocar cipher.encode() o cipher.
+//decode() según sea necesario y 
+//actualizar el resultado en la UI.
+
+let codigo= document.getElementById("ingresoTexto");
+let number= document.getElementById("Number");
+let result= document.getElementById("salidaTexto");
+let botonCifrado= document.getElementById("Cifrado");
+let botonDescifrado=document.getElementById("Descifrado");
+
+botonCifrado.addEventListener('click', cipher.encode());
+botonDescifrado.addEventListener('click', cipher.decode());
